bc3416988169493e21a6e4f2c6462d05f9a7212a
Aftab Ashrafi <ashrafiaftab0@gmail.com>
2020-03-13 16:26:47 +0530

    called led charging path

--- device.mk
+++ device.mk
@@ -31,7 +31,8 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-derp
+    $(LOCAL_PATH)/overlay-derp \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
