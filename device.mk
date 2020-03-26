37adb6e95220c8e94a6c7d1aaa835c697dfa96e3
Aftab Ashrafi <ashrafiaftab0@gmail.com>
2020-03-26 11:11:29 +0530

    Added path for custom doze

--- device.mk
+++ device.mk
@@ -28,7 +28,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-derp \
+    $(LOCAL_PATH)/overlay-bliss \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
