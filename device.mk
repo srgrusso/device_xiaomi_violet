24364f38cdb0f041d4aef1d28f57122d91986457
Aftab Ashrafi <ashrafiaftab0@gmail.com>
2020-03-28 18:24:21 +0530

    removed rro

--- device.mk
+++ device.mk
@@ -26,11 +26,6 @@ TARGET_SCREEN_WIDTH := 1080
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-reloaded \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 PRODUCT_PACKAGES += \
     NotchBarKiller
