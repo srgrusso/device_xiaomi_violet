1fa792c827474a2d6cc360a7dd02ab48ae0806c7
Aftab Ashrafi <ashrafiaftab0@gmail.com>
2020-04-26 11:19:40 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -28,12 +28,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-revengeos \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-revengeos
 
 PRODUCT_PACKAGES += \
     NotchBarKiller
